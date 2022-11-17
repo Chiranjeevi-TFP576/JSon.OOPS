e340a6084083f2615a4e0537ec2d166fc3358d76
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-11-17 16:14:51 +0530

    UC1_JSon.OOPS Problem

---
+++ ObjectOrientedProgram.Inventory/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ObjectOrientedProgram.Inventory;
+
+const string INVENTORY_DATA_FILE_PATH = @"D:\Bridgelabs\ObjectOrientedProgram.Inventory\ObjectOrientedProgram.Inventory\Inventorymanagement\Inventor.json";
+ {
+        Console.WriteLine(" Inventory Management Programme ");
+        
+        Inventory inventory = new Inventory();
+        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+
+ }
